d5634717da7e89065cedc0595bc8a76dad3c3cf9
Eva Malluta <eva.malluta14@gmail.com>
2022-07-04 15:06:51 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,66 @@
+// solution goes here
+type CommentData={
+    id:number
+    content:string
+    imageId:string
+}
+type Image={
+    id:number
+    title:string
+    likes:number
+    image:string
+    comments:CommentData[]
+}
+type State={
+    images:Image[]
+}
+let state:State ={
+    images: []
+}
+function getImagesFromServer(){
+fetch(`http://localhost:3333/images`)
+.then(resp=>resp.json())
+.then(getImagesFromServer=>{
+    state.images=getImagesFromServer
+    render()
+})
+}
+function render(){
+    let conteinerEl= document.querySelector<HTMLElement>(`.image-container`)
+    if(conteinerEl===null) return 
+    conteinerEl.textContent=""
+    for(let image of state.images){
+    let articleEl=document.createElement("article")
+    articleEl.className=("image-card")
+
+    let titleEl = document.createElement("h2")
+    titleEl.className = 'title'
+    titleEl.textContent = image.title
+
+    let imgEl=document.createElement("img")
+    imgEl.className=("image")
+    imgEl.src=image.image
+
+    let divEl=document.createElement("div")
+    divEl.className=("likes-section")
+    let spanEl=document.createElement("span")
+    spanEl.className=("likes")
+    spanEl.textContent=`${image.likes} likes`
+    let buttonEl=document.createElement("button")
+    buttonEl.className=("like-button")
+    buttonEl.textContent=("♥")
+    divEl.append(spanEl,buttonEl)
+
+    let ulEl=document.createElement("ul")
+    ulEl.className=("comments")
+    for(let coment of image.comments){
+    let liEl=document.createElement("li")
+    liEl.textContent=coment.content
+    ulEl.append(liEl)
+    }
+    articleEl.append(titleEl, imgEl,divEl ,ulEl)
+    conteinerEl.append(articleEl)
+    }
+}
+getImagesFromServer()
+render()
